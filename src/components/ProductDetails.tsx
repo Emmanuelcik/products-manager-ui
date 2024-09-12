a43fb18307706b960784a390fed3829f56c78f81
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-09-11 23:20:01 -0600

    added edition of a product

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Product } from "../types";
 
 import { formatCurrency } from "../utils";
@@ -6,6 +7,7 @@ type ProductDetails = {
   product: Product;
 };
 const ProductDetails = ({ product }: Product) => {
+  const navigate = useNavigate();
   const isAvailable = product.availability;
   return (
     <tr className="border-b ">
@@ -16,7 +18,21 @@ const ProductDetails = ({ product }: Product) => {
       <td className="p-3 text-lg text-gray-800">
         {isAvailable ? "Available" : "Unavailable"}
       </td>
-      <td className="p-3 text-lg text-gray-800 "></td>
+      <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center justify-center">
+          <button
+            onClick={() =>
+              navigate(`/products/${product.id}/edit`, { state: { product } })
+            }
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs"
+          >
+            Edit
+          </button>
+          <button className="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-xs">
+            Remove
+          </button>
+        </div>
+      </td>
     </tr>
   );
 };
